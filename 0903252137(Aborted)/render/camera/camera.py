42f3b190609d98304b14fcdd3815c98ef797af95
RRDDMC <rrdd.mcjava@gmail.com>
2025-03-16 19:37:27 +0100

    Add files via upload

---
+++ 0903252137(Aborted)/render/camera/camera.py
@@ -0,0 +1,68 @@
+import asyncio
+from render.camera.utils import *
+from world.generation import checkChunk
+
+
+async def cameraMove(data):
+    cameraPivot.position += mouse.relative.x * rotations[rotationNumber][1][0] * 0.25 * (cameraPivot.y + 51)
+    cameraPivot.position += mouse.relative.y * rotations[rotationNumber][1][1] * 0.25 * (cameraPivot.y + 51)
+    verifyWorldLimit()
+    checkChunk(data)
+
+
+async def setCameraToCenter(data):
+    cameraPivot.position = cameraPivot.position * Vec3(0, 1, 0) + Vec3(-11.5, 0, 8.5) - getCameraPosition().y * rotations[rotationNumber][0] * Vec3(1, 0, 1)
+    checkChunk(data)
+
+
+async def cameraZoomIn(data):
+    if cameraPivot.position * Vec3(0, 1, 0) > Vec3(0, -40, 0):
+        cameraPivot.position += rotations[rotationNumber][0]
+    verifyWorldLimit()
+    checkChunk(data)
+
+
+async def cameraZoomOut(data):
+    if cameraPivot.position * Vec3(0, 1, 0) < Vec3(0, -5, 0):
+        cameraPivot.position -= rotations[rotationNumber][0]
+    verifyWorldLimit()
+    checkChunk(data)
+
+
+async def cameraRotateRight(data):
+    global rotationNumber, goalRotation
+    rotationNumber += 1
+    if rotationNumber > 3:
+        rotationNumber = 0
+    goalRotation += Vec3(0, 90, 0)
+    checkChunk(data)
+
+
+async def cameraRotateLeft(data):
+    global rotationNumber, goalRotation
+    rotationNumber -= 1
+    if rotationNumber < 0:
+        rotationNumber = 3
+    goalRotation -= Vec3(0, 90, 0)
+    checkChunk(data)
+
+
+async def cameraRotateUpdate():
+    global goalRotation
+    if camera.rotation != goalRotation:
+        if goalRotation < camera.rotation:
+            camera.rotation -= Vec3(0, 5, 0)
+        else:
+            camera.rotation += Vec3(0, 5, 0)
+
+
+
+def verifyWorldLimit():
+    if (getCameraPosition() * Vec3(1, 0, 0)).x > worldLimit:
+        cameraPivot.position = cameraPivot.position * Vec3(0, 1, 1) + Vec3(worldLimit - 11.5, 0, 0)
+    elif (getCameraPosition() * Vec3(1, 0, 0)).x < -worldLimit:
+        cameraPivot.position = cameraPivot.position * Vec3(0, 1, 1) + Vec3(-worldLimit - 11.5, 0, 0)
+    if (getCameraPosition() * Vec3(0, 0, 1)).z > worldLimit:
+        cameraPivot.position = cameraPivot.position * Vec3(1, 1, 0) + Vec3(0, 0, worldLimit + 8.5)
+    elif (getCameraPosition() * Vec3(0, 0, 1)).z < -worldLimit:
+        cameraPivot.position = cameraPivot.position * Vec3(1, 1, 0) + Vec3(0, 0, -worldLimit + 8.5)
---
+++ 0903252137(Aborted)/tests/texturesTest.py
@@ -0,0 +1,72 @@
+from ursina import *
+
+BLOCKS = ["textures/model.png", "model/textureTemplate.png", "textures/dirt.png", "textures/grassNormalStep.png",
+          "textures/grassSummer.png", "textures/grassWinter.png", "textures/grassSpring.png",
+          "textures/grassAutumn.png", "textures/water.gif"]
+
+
+class TextureTest():
+    def __init__(self, blocks):
+        global input
+        self.app = Ursina(development_mode=True, use_ingame_console=True)
+
+        def input(key):
+            print(key)
+            if key in ['left arrow', 'left arrow hold']:
+                self.relativeBlock(-1)
+            if key in ['right arrow', 'right arrow hold']:
+                self.relativeBlock(1)
+            if key == 'backspace':
+                self.running = False
+            if key == 'ecape':
+                self.running = False
+
+        self.BLOCK_MODEL_POSITION = Vec3(-0.74, 0.32, 0)
+        self.BLOCK_MODEL_SCALE = Vec3(0.3, 0.3, 0.3)
+
+        self.blocks = blocks
+        self.actualBlock = -1
+
+        self.actualBlockText = Text('')
+        self.actualBlockText.position = window.top_left
+
+        self.blockRender = Entity(model='model/block.obj')
+        self.blockModel = Entity(model='quad', parent=camera.ui, position=self.BLOCK_MODEL_POSITION,
+                                 scale=self.BLOCK_MODEL_SCALE)
+        self.blockModelDefault = Entity(model='quad', texture='model/textureTemplate.png', parent=camera.ui,
+                                        position=self.BLOCK_MODEL_POSITION, scale=self.BLOCK_MODEL_SCALE)
+
+        window.title = 'Texturing Test Interface'  # The window title
+        window.borderless = False  # Show a border
+        window.fullscreen = False  # Do not go Fullscreen
+        window.exit_button.visible = True  # Show the in-game red X that loses the window
+        window.fps_counter.enabled = True  # Show the FPS (Frames per second) counter
+        EditorCamera()
+
+        self.running = True
+        self.relativeBlock(1)
+
+    def relativeBlock(self, step):
+        lastTexture = self.blockRender.texture
+        self.actualBlock += step
+        if self.actualBlock < 0:
+            self.actualBlock = len(self.blocks) - 1
+        elif self.actualBlock > len(self.blocks) - 1:
+            self.actualBlock = 0
+        self.actualBlockText.text = self.blocks[self.actualBlock]
+        self.blockRender.texture = self.blocks[self.actualBlock]
+        self.blockModel.texture = self.blocks[self.actualBlock]
+        if self.blockRender.texture == lastTexture:
+            self.blockRender.texture = self.blocks[0]
+            self.blockModel.texture = self.blocks[0]
+
+    def run(self):
+        while self.running:
+            self.app.step()
+
+
+if __name__ == "__main__":
+    import os
+
+    os.chdir("..")
+TextureTest(BLOCKS).run()
