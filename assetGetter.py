7df06ced9fcdd3003d9b1839169d09b64a3e052d
RRDDMC <rrdd.mcjava@gmail.com>
2025-03-21 20:35:56 +0100

    Update assetGetter.py

--- assetGetter.py
+++ assetGetter.py
@@ -5,7 +5,7 @@ class getter():
     def __init__(self, url, distantPath="", localPath="", manifest="versions.txt"):
         self.url = url
         self.distantPath = distantPath
-        self.localPath = localPath
+        self.localPath = "{}/{}".format(os.getcwd(), localPath)
 
         self.versions = {}
         versionManifest = self.getStringFile(manifest)
@@ -15,24 +15,23 @@ class getter():
             if len(line) == 2:
                 self.versions[line[0]] = line[1]
     def getStringFile(self, filePath):
-        print(self.url + self.distantPath + filePath)
         response = requests.get(self.url + self.distantPath + filePath)
         if response.status_code != 200:
             raise Exception("{} -> {}".format(response.status_code, response.content))
         else:
             return response.text
     def getBytesFile(self, filePath):
-        print(self.url + self.distantPath + filePath)
         response = requests.get(self.url + self.distantPath + filePath)
         if response.status_code != 200:
             raise Exception("{} -> {}".format(response.status_code, response.content))
         else:
             return response.content
-    def getAssets(self, version):
+    def getAssets(self, version, forceUpdate=False):
+        forceNextUpdate = False
         if not os.path.exists(self.localPath):
             os.mkdir(self.localPath)
         self.distantPath = ""
-        self.localPath = ""
+        localPath = ""
 
         manifest = self.getStringFile(self.versions[version])
         manifest = manifest.split("\n")
@@ -44,27 +43,36 @@ class getter():
             elif line[0] == "-":
                 if not f:
                     raise SyntaxError("No file name befor {}".format(line))
-                fContent = self.getBytesFile(line[1:])
-                if not os.path.exists(self.localPath + f):
-                    f = open(f, "wb")
+                if not os.path.exists(self.localPath + localPath + f) or forceUpdate or forceNextUpdate:
+                    fContent = self.getBytesFile(line[1:])
+                    f = open(self.localPath + localPath + f, "wb")
                     f.write(fContent)
                     f.close()
+                    if forceNextUpdate:
+                    	forceNextUpdate = False
+                f = None
             elif line[0] == "#":
-                self.localPath = line[1:]
-                if not os.path.exists(self.localPath):
-                    os.mkdir(self.localPath)
+                localPath = line[1:]
+                if not os.path.exists(self.localPath + localPath):
+                    os.mkdir(self.localPath + localPath)
             elif line[0] == "=":
                 self.distantPath = line[1:]
             else:
                 if line[-1] == ":":
                     f = line[:-1]
+                    if f[0] == "@":
+                    	forceNextUpdate = True
                 else:
                     f = line
-                    fContent = self.getBytesFile(self.localPath + f)
-                    f = open(f, "wb")
-                    f.write(fContent)
-                    f.close()
-                    f = None
+                    if f[0] == "@":
+                    	f = f[1:]
+                    	forceNextUpdate= True
+                    if not os.path.exists(self.localPath + localPath + f) or forceUpdate or forceNextUpdate:
+                    	fContent = self.getBytesFile(f)
+                    	f = open(self.localPath + localPath + f, "wb")
+            	        f.write(fContent)
+            	        f.close()
+            	        f = None
+            	        if forceNextUpdate: forceNextUpdate = False
 
 a = getter("https://raw.githubusercontent.com/RRDDMC/YOG/main/assets/", localPath="data/")
-a.getAssets("0.0.1")
